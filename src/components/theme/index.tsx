a8a90c247d005a59179de361d382f00908f30181
thiagowgarcia <thiagowgarcia@gmail.com>
2022-06-15 18:11:24 -0300

    criação de theme

---
+++ src/components/theme/index.tsx
@@ -0,0 +1,24 @@
+import { ReactNode } from 'react';
+import * as C from './styles';
+
+type Props = {
+  children: ReactNode;
+}
+
+export const Theme = ({ children }: Props) => {
+  return (
+    <C.Container>
+      <C.Area>
+        <C.Steps>
+          <C.Sidebar>
+
+          </C.Sidebar>
+          <C.Page>
+            {children}
+          </C.Page>
+        </C.Steps>
+      </C.Area>
+    </C.Container>
+  )
+
+}
---
+++ src/components/theme/styles.ts
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  background-color: #020244A;
+  color: #fff;
+  min-height: 100vh;
+`;
+
+export const Area = styled.div`
+
+`
+
+export const Steps = styled.div`
+
+`
+
+export const Sidebar = styled.div`
+
+`
+
+export const Page = styled.div`
+
+`
