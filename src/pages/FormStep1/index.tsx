3e06bcd6b458573370eca95ddb1dd0cb942fe3dd
thiagowgarcia <thiagowgarcia@gmail.com>
2022-06-23 16:54:47 -0300

    step1

--- src/pages/FormStep1/index.tsx
+++ src/pages/FormStep1/index.tsx
@@ -1,10 +1,37 @@
+import { useHistory } from 'react-router-dom';
 import * as C from './styles';
+import { useForm } from '../../contexts/FormContext';
 import { Theme } from '../../components/Theme'
 
 export const FormStep1 = () => {
+  const history = useHistory();
+  const { state, dispatch } = useForm();
+
+  const handleNextStep = () => {
+    history.push('/step2');
+  }
+
   return (
-    <C.Container>
-      <h1>Proj Thiago no jeito man</h1>
-    </C.Container>
+    <Theme>
+      <C.Container>
+        <p>Passo 1/3</p>
+        <h1>Vamos começar com seu nome</h1>
+        <p>Preencha o campo abaixo com seu nome completo.</p>
+
+        <hr />
+
+        <label>
+          Seu nome completo
+          <input
+            type="text"
+            autoFocus
+
+          />
+        </label>
+
+        <button onClick={handleNextStep}>próximo</button>
+
+      </C.Container>
+    </Theme>
   );
 }
