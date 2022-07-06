4695831461f4c75d535b28532973e22fac7a6ebc
thiagowgarcia <thiagowgarcia@gmail.com>
2022-07-06 09:56:46 -0300

    Inicio Sidebar

--- src/components/SideBarItem/styles.ts
+++ src/components/SideBarItem/styles.ts
@@ -1,3 +1,21 @@
 import styled from "styled-components";
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  margin: 30px 0;
+  cursor: pointer;
+  
+	a {
+    display: flex;
+    align-items: center;
+    text-decoration: none;
+  }
+`;
+
+export const Info = styled.div`
+    flex: 1;
+    margin-right: 20px; 
+  
+  `;
+
+
+
